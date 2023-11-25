eacb323136162260839127c0079c01a31dda8466
HiepThanhTran <hieptt.2003@gmail.com>
2023-11-26 04:56:14 +0700

    bug fixed

--- company/models.py
+++ company/models.py
@@ -7,12 +7,12 @@ from home.models import Account
 class Company(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE, primary_key=True)
     company_name = models.CharField(max_length=50, null=False, unique=True)
-    description = RichTextField()
-    address = models.CharField(max_length=100, null=True)
-    number_of_employees = models.IntegerField(null=True, default=0)
-    social_link = models.CharField(max_length=254, null=True)
-    industry = models.CharField(max_length=50, null=True)
-    followers = models.IntegerField(default=0)
+    description = RichTextField(null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    number_of_employees = models.IntegerField(null=True, default=0, blank=True)
+    social_link = models.CharField(max_length=254, null=True, blank=True)
+    industry = models.CharField(max_length=50, null=True, blank=True)
+    followers = models.IntegerField(default=0, blank=True)
     slug = models.SlugField(default="", null=True, blank=True)
 
     def __str__(self):
--- cv_management/models.py
+++ cv_management/models.py
@@ -5,7 +5,7 @@ from user.models import User
 class CurriculumVitae(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cvs')
     name = models.CharField(max_length=50, null=False)
-    image = models.ImageField(upload_to='upload/%Y/%m')
+    image = models.ImageField(upload_to='upload/%Y/%m', null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
